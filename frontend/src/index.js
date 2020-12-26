ec6997b82400be5f21adfcc932b5f76468da63ed
endalk2 <eb808826@gmail.com>
2020-12-26 11:52:27 +0300

    [feature] :rocket: Final commit with all necessary files :rocket:
    The included and changed files are as follows:
    * docker setup for both the frontend and the backend
    * code quality test for the backend using flake8
    * CI test using GitHub actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+/**
+ * Styles
+ * There are two styles imported here. 
+ * 
+ * The first one is sass file from scss
+ * directory. If you use sass in your project start in that directory and add
+ * your styles in the main.scss file.
+ * 
+ * The second one is css file from the css directory if you are using css in
+ * in your project it is already imported and you can start there
+ */
+import "./static/sass/main.scss";
+import "./static/css/main.css";
+
+/**
+ * Additional js file imports are added here
+ */
+import "./static/js/index";
 
 ReactDOM.render(
   <React.StrictMode>
